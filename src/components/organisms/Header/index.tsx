db8fbd73c6df8c7e23fb00c554cbd6249b8fd518
Minh Tuấn <tuanbm.work@gmail.com>
2024-11-23 11:31:42 +0700

    feat:header

--- src/components/organisms/Header/index.tsx
+++ src/components/organisms/Header/index.tsx
@@ -1,5 +1,25 @@
+import { NavLink, Link } from "react-router-dom";
+import "./styles.scss";
+
+const NAVLINK = {
+  login: "/login",
+  register: "/register",
+};
+
 type HeaderProps = {};
 
 export const Header = ({}: HeaderProps) => {
-  return <header className="container">Header</header>;
+  return (
+    <header className="o-header">
+      <div className="container">
+        <Link to="/" className="o-header_logo">
+          <img src="images/logo.svg" alt="" />
+        </Link>
+        <ul className="o-header_nav">
+          <NavLink to={NAVLINK.register}>Register</NavLink>
+          <NavLink to={NAVLINK.login}>Log In</NavLink>
+        </ul>
+      </div>
+    </header>
+  );
 };
